e732b1d632fb4ad0c228defd0b3b799e4d5ff5fb
asikin-banyaksolutions <Dhiyaul.Asikin@banyaksolutions.com>
2025-10-08 08:07:47 +0700

    Fix scenario login

--- Scripts/Testcase_login/Script1529986178839.groovy
+++ Scripts/Testcase_login/Script1529986178839.groovy
@@ -3,6 +3,20 @@ import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.util.internal.PathUtil as PathUtil
 import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 
 Mobile.startApplication('D:\\titip\\test\\gvm-staging.apk', true)
 
@@ -21,6 +35,18 @@ Mobile.doubleTap(findTestObject('Registration/Masuk button'), 0)
 Mobile.delay(3, FailureHandling.STOP_ON_FAILURE)
 
 Mobile.tap(findTestObject('Registration/Profile section'), 0)
+
+Mobile.delay(3, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Login/Email Input'), 0)
+
+Mobile.setText(findTestObject('Login/Email Set'), 'asik@email.com', 0)
+
+Mobile.setText(findTestObject('Login/Password Set'), 'Asikin01!', 0)
+
+Mobile.pressBack()
+
+Mobile.tap(findTestObject('Login/Button Masuk'), 0)
 
 Mobile.delay(3, FailureHandling.STOP_ON_FAILURE)
 
